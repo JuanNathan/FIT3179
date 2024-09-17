9efdd734777128d72b464eb359be6e6dc3aa0d3e
Juan Nathan <140228535+JuanNathan@users.noreply.github.com>
2024-09-17 17:55:23 +0800

    Add JS file

---
+++ natural_disaster_risk.js
@@ -0,0 +1,4 @@
+var vg_1 = "natural_disaster_risk.vg.json";
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
